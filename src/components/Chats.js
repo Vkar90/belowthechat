a178c0c67006e44b78d7bda4c192b8919ddef68e
Vkar90 <kardamitsis.e@gmail.com>
2021-07-27 12:27:25 +0300

    Finishing development

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,86 +1,96 @@
-import React, {useRef, useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom';
+import React, { useRef, useState, useEffect } from "react"
+
+import axios from 'axios'
+import { useHistory } from "react-router-dom"
 import { ChatEngine } from 'react-chat-engine'
-import { auth } from '../firebase'
-import { useAuth } from '../contexts/AuthContext';
-import axios from 'axios';
 
-const Chats = () => {
-    const history = useHistory()
-    const { user } = useAuth()
-    const [loading, setLoading] = useState(true)
-    
-    const handleLogout = async () => {
-        await auth.signOut()
+import { useAuth } from "../contexts/AuthContext"
 
-        history.push('/')
+import { auth } from "../firebase"
+
+export default function Chats() {
+  const didMountRef = useRef(false)
+  const [ loading, setLoading ] = useState(true)
+  const { user } = useAuth()
+  const history = useHistory()
+
+  async function handleLogout() {
+    await auth.signOut()
+    history.push("/")
+  }
+
+  async function getFile(url) {
+    let response = await fetch(url);
+    let data = await response.blob();
+    return new File([data], "test.jpg", { type: 'image/jpeg' });
+  }
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true
+
+      if (!user || user === null) {
+        history.push("/")
+        return
+      }
+      
+      // Get-or-Create should be in a Firebase Function
+      axios.get(
+        'https://api.chatengine.io/users/me/',
+        { headers: { 
+          "project-id": process.env.REACT_APP_CHAT_ENGINE_ID,
+          "user-name": user.email,
+          "user-secret": user.uid
+        }}
+      )
+
+      .then(() => setLoading(false))
+
+      .catch(e => {
+        let formdata = new FormData()
+        formdata.append('email', user.email)
+        formdata.append('username', user.email)
+        formdata.append('secret', user.uid)
+
+        getFile(user.photoURL)
+        .then(avatar => {
+          formdata.append('avatar', avatar, avatar.name)
+
+          axios.post(
+            'https://api.chatengine.io/users/',
+            formdata,
+            { headers: { "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY }}
+          )
+          .then(() => setLoading(false))
+          .catch(e => console.log('e', e.response))
+        })
+      })
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     }
+  }, [user, history])
+  
 
-    const getFile = async (url) => {
-        const response = await fetch(url)
-        const data = await response.blob()
+  if (!user || loading) return <div />
 
-        return new File([data], "userPhoto.jpg", { type: 'image/jpeg'})
-    }
-
-    useEffect(() => {
-        if(!user || user === null)  {
-            history.push('/')
-
-            return
-        }
-
-        axios.get('https://api.chatengine.io/users/me', {
-            headers: {
-                "project-id": "8d2a6aae-a88b-48cd-a846-aa5534fdb3d2",
-                "user-name": user.email , 
-                "user-secret": user.uid,
-            }
-        })
-        .then( () => {
-            setLoading(false)
-        })
-        .catch(() => {
-            let formdata = new FormData()
-            formdata.append('email', user.email)
-            formdata.append('username', user.email)
-            formdata.append('secret', user.uid)
-
-            getFile(user.photoURL)
-                .then((avatar) => {
-                    formdata.append('avatar', avatar, avatar.name)
-
-                    axios.post('https://api.chatengine.io/users/', 
-                    formdata, 
-                    {headers: { "private-key ": "17717319-122d-473d-9968-b216b4ecf55f"}})
-                    .then(() => {
-                        setLoading(false)
-                    })
-                    .catch((error) => console.log(error))
-                })
-        })
-    }, [user, history])
-
-    if(!user || loading ) return 'Loading...'
-
-    return (
-        <div className='chats-page'>
-            <div className='nav-bar'>
-                <div className='logo-tab'>
-                    Below The Chat
-                </div>
-                <div onClick={handleLogout} className="logout-tab"> 
-                    Logout
-                </div>
-            </div>
-            <ChatEngine
-                height="calc(100vh - 66px)"
-                projectID='1974e293-8363-4899-ba99-581c2548db4d'
-                userName= {user.email}
-                userSecret={user.uid}
-            />
+  return (
+    <div className='chats-page'>
+      <div className='nav-bar'>
+        <div className='logo-tab'>
+          Unichat
         </div>
-    )
-}
 
-export default Chats;
+        <div onClick={handleLogout} className='logout-tab'>
+          Logout
+        </div>
+      </div>
+
+      <ChatEngine 
+        height='calc(100vh - 66px)'
+        projectID= {process.env.REACT_APP_CHAT_ENGINE_ID}
+        userName={user.email}
+        userSecret={user.uid}
+      />
+    </div>
+  )
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from "react"
+
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
-import "firebase/app"
-import { auth } from '../firebase'
-import firebase from 'firebase/app'
 
-const Login = () => {
-    return (
-        <div id="login-page">
-            <div id="login-card">
-                <h2>Welcome to Below The Chat!</h2>
-                <div
-                    className="login-button google"
-                    onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
-                >
-                    <GoogleOutlined /> Sign in with Google
-                </div>
+import firebase from "firebase/app"
 
-                <br /> <br />
-                <div
-                    className="login-button facebook"
-                    onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
-                >
-                    <FacebookOutlined /> Sign in with Facebook
-                </div>
-            </div>
+import { auth } from "../firebase"
 
+export default function Login() {
+  return (
+    <div id='login-page'>
+      <div id='login-card'>
+        <h2>Welcome to Unichat!</h2>
+
+        <div
+          className='login-button google'
+          onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+        >
+          <GoogleOutlined /> Sign In with Google
         </div>
-    )
-}
 
-export default Login
+        <br/><br/>
+
+        <div
+          className='login-button facebook'
+          onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider()) }
+        >
+          <FacebookOutlined /> Sign In with Facebook
+        </div>
+      </div>
+    </div>
+  )
+}
