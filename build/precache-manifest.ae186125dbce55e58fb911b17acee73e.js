6d781539d5b7f9d86d6b21f3198ba8498bca46b1
Vkar90 <kardamitsis.e@gmail.com>
2021-07-27 12:36:07 +0300

    Change h1 to Chats.js

---
+++ build/precache-manifest.ae186125dbce55e58fb911b17acee73e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29651fc3367338d831f300132771bf03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b14d8c9715b0211fcca",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "3b13bed470a8dfa3719b",
+    "url": "/static/js/2.359553d2.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.359553d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b14d8c9715b0211fcca",
+    "url": "/static/js/main.6619cc5d.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
